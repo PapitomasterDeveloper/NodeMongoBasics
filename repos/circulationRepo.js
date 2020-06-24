31eb4c4b875e67b7e674840c12a898130f077595
Miguel Angel Cavazos Reyes <miguel.cavazos@ad.infosys.com>
2020-06-23 21:08:15 -0500

    Added try / catch / finally blocks to better assert errors with Mongo client when GETing data

--- repos/circulationRepo.js
+++ repos/circulationRepo.js
@@ -4,6 +4,20 @@ function circulationRepo() {
 	const url = 'mongodb://localhost:27017';
 	const dbName = 'circulation';
 
+	function get() {
+		return new Promise(async (resolve, reject) => {
+			const client = new MongoClient(url);
+			try {
+				await client.connect();
+				const items = db.collection('newspapers').find();
+
+				resolve(await items.toArray());
+			} catch (error) {
+				reject(error);
+			}
+		});
+	}
+
 	function loadData(data) {
 		return new Promise(async (resolve, reject) => {
 			// For each new operation, there needs to be a new
@@ -26,5 +40,5 @@ function circulationRepo() {
 	}
 
 	// Object available with the loadData resolved result or rejected error
-	return { loadData }
+	return { loadData, get }
 }
